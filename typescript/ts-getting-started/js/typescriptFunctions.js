2ccb023b257d2ea314d3666843e50167b6836148
MisterScriptster <mistersinister1@protonmail.com>
2020-04-23 15:28:41 +0200

    chapter 3: typescript functions

--- typescript/ts-getting-started/js/typescriptFunctions.js
+++ typescript/ts-getting-started/js/typescriptFunctions.js
@@ -1,2 +1,25 @@
 "use strict";
+function someFunc(score, message) {
+    return "The question mark indicates that the parameter is optional. This implies\n  that in typescript all parameters are considered required unless flagged otherwise.\n  The (): string is the return parameter of the function";
+}
+;
+function sendGreeting(greeting) {
+    if (greeting === void 0) { greeting = "default variable value"; }
+    var commentary;
+    commentary = "We can assign a default value for the parameter, and this way we can call the\n  function without passing anything. Adding a default value makes a parameter optional.\n  Void implies that the function has no return parameters";
+    console.log(commentary);
+}
+;
+function funWithFlags(flag) {
+    var noImplicitAny;
+    noImplicitAny = "This flag requires all function parameters to be annotated. Function parameters\n  that are not anotated are of type \"any\", which raises a compiler error when using this flag";
+    console.log(noImplicitAny);
+}
+;
+var logMessage = function (message) {
+    console.log("The " + message + " needs to be in brackets because of the type anotation, \n  otherwise it would raise an error");
+};
+var logError = function (err) {
+    console.error(err);
+};
 //# sourceMappingURL=typescriptFunctions.js.map
--- typescript/ts-getting-started/src/typescriptFunctions.ts
+++ typescript/ts-getting-started/src/typescriptFunctions.ts
@@ -1,5 +1,31 @@
 //Adding type annotations to functions
+function someFunc(score: number, message?: string): string{
+  return `The question mark indicates that the parameter is optional. This implies
+  that in typescript all parameters are considered required unless flagged otherwise.
+  The (): string is the return parameter of the function`;
+};
+
+function sendGreeting(greeting: string = "default variable value"): void{
+  let commentary: string;
+  commentary = `We can assign a default value for the parameter, and this way we can call the
+  function without passing anything. Adding a default value makes a parameter optional.
+  Void implies that the function has no return parameters`;
+  console.log(commentary);
+};
+
+function funWithFlags(flag: string): void {
+  let noImplicitAny: string;
+  noImplicitAny = `This flag requires all function parameters to be annotated. Function parameters
+  that are not anotated are of type "any", which raises a compiler error when using this flag`;
+  console.log(noImplicitAny);
+};
 
 //Using arrow functions 
+const logMessage = (message: string): void => {
+  console.log(`The ${message} needs to be in brackets because of the type anotation, 
+  otherwise it would raise an error`);
+};
 
-//Declaring function types
+const logError = (err: string): void =>{
+  console.error(err);
+};
