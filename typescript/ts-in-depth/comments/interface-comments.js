5d7be172cd5e173b35461dd4cbfdb62daebc41ee
MisterScriptster <mistersinister1@protonmail.com>
2020-04-26 11:28:01 +0200

    Finish chapter 6 update - allways commit from the root

--- typescript/ts-in-depth/comments/interface-comments.js
+++ typescript/ts-in-depth/comments/interface-comments.js
@@ -13,6 +13,31 @@ function FlyOverWater(bird) {
 ;
 FlyOverWater(probablyADuck); //works!!
 function interfaceDefinition() {
-    return "All of the properties and methods on an interface are required for the object \n  that implements the interface. \n  It is also possible for an interface to have optional properties denoted with ?\n  Methods can be specified by giving the method a name and a signature";
+    return "All of the properties and methods on an interface are required for the object \n  that implements the interface. \n  It is also possible for an interface to have optional properties denoted with ?\n  Methods can be specified by giving the method a name and a signature\n  If a method is optional on the interface, you must perform a check of it's\n  existence before calling the method";
 }
 ;
+///////////////////////////////////
+// Interfaces for Function Types //
+function exCreateCustomerID(name, id) {
+    return name + id;
+}
+;
+;
+var IdGenerator;
+IdGenerator = exCreateCustomerID;
+;
+;
+;
+;
+var ElementarySchoolLibrarian = /** @class */ (function () {
+    function ElementarySchoolLibrarian() {
+    }
+    ElementarySchoolLibrarian.prototype.doWork = function () {
+        console.log("The implements keywords specifies that the class will be the implmentation\n    of the interface");
+    };
+    ;
+    return ElementarySchoolLibrarian;
+}());
+;
+var kidsLibrarian = new ElementarySchoolLibrarian;
+kidsLibrarian.doWork();
--- typescript/ts-in-depth/comments/interface-comments.ts
+++ typescript/ts-in-depth/comments/interface-comments.ts
@@ -7,6 +7,7 @@
   -The compiler can then check our implemntations of those interfaces
   -They can be extendend and composed from other interfaces
   -They do not contain implementation details
+  -They do not get compiled to JS
 */
 
 interface Duck{
@@ -29,8 +30,8 @@ function FlyOverWater(bird: Duck): string{
 
 FlyOverWater(probablyADuck); //works!!
 
-//Defining an Interface
-
+///////////////////////////
+// Defining an Interface //
 
 interface Car {
   model: string,
@@ -45,5 +46,60 @@ function interfaceDefinition(): string{
   return `All of the properties and methods on an interface are required for the object 
   that implements the interface. 
   It is also possible for an interface to have optional properties denoted with ?
-  Methods can be specified by giving the method a name and a signature`;
+  Methods can be specified by giving the method a name and a signature
+  If a method is optional on the interface, you must perform a check of it's
+  existence before calling the method`;
 };
+
+
+///////////////////////////////////
+// Interfaces for Function Types //
+
+function exCreateCustomerID(name: string, id: number): string{
+  return name + id;
+};
+
+//function type interface
+interface StringGenerator {
+  (chars: string, nums: number): string;
+};
+
+let IdGenerator: StringGenerator;
+IdGenerator = exCreateCustomerID;
+
+
+//////////////////////////
+// Extending Interfaces //
+
+interface LibraryResource {
+  catalogNumber: number;
+};
+
+interface Book {
+  title: string;
+};
+
+interface Encyclopedia extends LibraryResource, Book {
+  volume: number;
+};
+
+
+/////////////////
+// Class Types //
+
+interface Librarian {
+  doWork: () => void;
+};
+
+class ElementarySchoolLibrarian implements Librarian {
+  doWork() {
+    console.log(`The implements keywords specifies that the class will be the implmentation
+    of the interface`)
+  };
+};
+
+let kidsLibrarian: Librarian = new ElementarySchoolLibrarian;
+kidsLibrarian.doWork();
+
+
+
